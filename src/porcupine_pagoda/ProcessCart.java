2ed3d59b940492e4e66d51b3f0a780ffd69d6c59
Armando Gonzalez <armangonzal@gmail.com>
2019-07-26 13:19:36 -0500

    Update ProcessCart.java
    quantity is already int

--- src/porcupine_pagoda/ProcessCart.java
+++ src/porcupine_pagoda/ProcessCart.java
@@ -79,7 +79,7 @@ public class ProcessCart extends HttpServlet {
 					item.setColor(rsProd.getString("product_color"));
 					item.setSku(rsProd.getString("product_sku"));
 					item.setWeight(rsProd.getDouble("product_weight"));
-					item.setNbitems(String.parseInt(quantity));
+					item.setNbitems(quantity);
 					
 					
 					itemSt.close();
